763292efb7a4cf30116719e92461f0ce110cee1a
zeniconcombres <email.me.inez@gmail.com>
2022-11-29 13:35:43 +1100

    Updating installation test PSSPY script and documentation details in .md

--- test_psse34_psspy_install.py
+++ test_psse34_psspy_install.py
@@ -1,17 +1,4 @@
-"""
-Author: Inez Zheng @zeniconcombres
-Date Created: 2022-11-14
-
-Script created to run PSSE studies and generate 
-basic scripts that can be used with the PSSE Xplore34 
-free version of the software.
-"""
-
 import os, sys, glob
-import matplotlib
-import numpy as np
-import pandas as pd
-from pandas import DataFrame as df
 
 # consider adding PSSE_PATH as a system var to avoid path issues
 PSSE_PATH = r'C:\Program Files (x86)\PTI\PSSEXplore34\PSSBIN'
@@ -21,10 +8,12 @@ sys.path.append(PSSE_PSSPY_PATH)
 os.environ['PATH'] += ';' + PSSE_PATH
 os.environ['PATH'] += ';' + PSSE_PSSPY_PATH
 
+# will detect a library path mismatch
 fns = glob.glob(os.path.join(PSSE_PSSPY_PATH, 'psspy.*'))
 if len(fns) == 0:
    print("Who moved my libraries")
 
 import psspy
 
+# if the below executes correctly without fail then PSSE has been installed correctly
 psspy.psseinit(100)
